0ac6e5410163288d00006be8c7ada9f9f743c842
Mohd Ashif Ali <ali.mashifali.ashif14@gmail.com>
2017-11-16 18:58:06 +0530

    Updated springboot with docker and swagger integration

--- src/main/java/com/tcs/Application.java
+++ src/main/java/com/tcs/Application.java
@@ -3,6 +3,13 @@ package com.tcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 
@@ -10,11 +17,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	
 	
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+				.select()                
+				.apis(RequestHandlerSelectors.basePackage("com.tcs.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
 
